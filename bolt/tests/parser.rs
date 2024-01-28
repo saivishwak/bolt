84bc4ddd0736bd1252bc002d1951785c89cb9576
saivishwak <saivishwak40@gmail.com>
2024-01-28 19:06:03 +0530

    add boolean expr tests

--- bolt/tests/parser.rs
+++ bolt/tests/parser.rs
@@ -10,7 +10,7 @@ use bolt::{
     parser::{
         ast::{
             self, BinaryExpression, Boolean, Expression, ExpressionStatement, Identifier,
-            IntegerLiteral, PrefixExpression, Statement,
+            IntegerLiteral, LetStatement, PrefixExpression, Statement,
         },
         parser::Parser,
     },
@@ -492,6 +492,218 @@ fn test_operator_precedence_parsing() {
                         line: 0,
                     },
                     value: 2.0,
+                }),
+            }),
+        },
+    ];
+    let size = tests.len();
+    for i in 0..size {
+        let mut parser = Parser::new(tests[i]);
+        let p = parser.parse_program();
+        match p {
+            Ok(res) => {
+                let stmt: &Box<dyn Statement> = &res.stmts[0];
+                assert_eq!(format!("{:?}", stmt), format!("{:?}", expected_results[i]));
+            }
+            Err(e) => {
+                print!("Error - {:?}", e.message);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_boolean_statement() {
+    let tests = ["let a = true;", "let b = false"];
+    let expected_results = vec![
+        LetStatement {
+            token: Token {
+                token_type: TokenType::LET,
+                literal: String::from("let"),
+                line: 0,
+            },
+            identifier: Identifier {
+                token: Token {
+                    token_type: TokenType::IDENTIFIER,
+                    literal: String::from("a"),
+                    line: 0,
+                },
+                value: String::from("a"),
+            },
+            value: Box::new(Boolean {
+                token: Token {
+                    token_type: TokenType::TRUE,
+                    literal: String::from("true"),
+                    line: 0,
+                },
+                value: true,
+            }),
+        },
+        LetStatement {
+            token: Token {
+                token_type: TokenType::LET,
+                literal: String::from("let"),
+                line: 0,
+            },
+            identifier: Identifier {
+                token: Token {
+                    token_type: TokenType::IDENTIFIER,
+                    literal: String::from("b"),
+                    line: 0,
+                },
+                value: String::from("b"),
+            },
+            value: Box::new(Boolean {
+                token: Token {
+                    token_type: TokenType::FALSE,
+                    literal: String::from("false"),
+                    line: 0,
+                },
+                value: false,
+            }),
+        },
+    ];
+    let size = tests.len();
+    for i in 0..size {
+        let mut parser = Parser::new(tests[i]);
+        let p = parser.parse_program();
+        match p {
+            Ok(res) => {
+                let stmt: &Box<dyn Statement> = &res.stmts[0];
+                assert_eq!(format!("{:?}", stmt), format!("{:?}", expected_results[i]));
+            }
+            Err(e) => {
+                print!("Error - {:?}", e.message);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_boolean_expression() {
+    let tests = ["true", "false", "5 < 8 == true;", "3 > 4 == false;"];
+    let expected_results = vec![
+        ExpressionStatement {
+            token: Token {
+                token_type: TokenType::TRUE,
+                literal: String::from("true"),
+                line: 0,
+            },
+            value: Box::new(Boolean {
+                token: Token {
+                    token_type: TokenType::TRUE,
+                    literal: String::from("true"),
+                    line: 0,
+                },
+                value: true,
+            }),
+        },
+        ExpressionStatement {
+            token: Token {
+                token_type: TokenType::FALSE,
+                literal: String::from("false"),
+                line: 0,
+            },
+            value: Box::new(Boolean {
+                token: Token {
+                    token_type: TokenType::FALSE,
+                    literal: String::from("false"),
+                    line: 0,
+                },
+                value: false,
+            }),
+        },
+        ExpressionStatement {
+            token: Token {
+                token_type: TokenType::INT,
+                literal: String::from("5"),
+                line: 0,
+            },
+            value: Box::new(BinaryExpression {
+                token: Token {
+                    token_type: TokenType::EQ,
+                    literal: String::from("=="),
+                    line: 0,
+                },
+                operator: String::from("=="),
+                left: Rc::new(Box::new(BinaryExpression {
+                    token: Token {
+                        token_type: TokenType::LT,
+                        literal: String::from("<"),
+                        line: 0,
+                    },
+                    operator: String::from("<"),
+                    left: Rc::new(Box::new(IntegerLiteral {
+                        token: Token {
+                            token_type: TokenType::INT,
+                            literal: String::from("5"),
+                            line: 0,
+                        },
+                        value: 5.0,
+                    })),
+                    right: Box::new(IntegerLiteral {
+                        token: Token {
+                            token_type: TokenType::INT,
+                            literal: String::from("8"),
+                            line: 0,
+                        },
+                        value: 8.0,
+                    }),
+                })),
+                right: Box::new(Boolean {
+                    token: Token {
+                        token_type: TokenType::TRUE,
+                        literal: String::from("true"),
+                        line: 0,
+                    },
+                    value: true,
+                }),
+            }),
+        },
+        ExpressionStatement {
+            token: Token {
+                token_type: TokenType::INT,
+                literal: String::from("3"),
+                line: 0,
+            },
+            value: Box::new(BinaryExpression {
+                token: Token {
+                    token_type: TokenType::EQ,
+                    literal: String::from("=="),
+                    line: 0,
+                },
+                operator: String::from("=="),
+                left: Rc::new(Box::new(BinaryExpression {
+                    token: Token {
+                        token_type: TokenType::GT,
+                        literal: String::from(">"),
+                        line: 0,
+                    },
+                    operator: String::from(">"),
+                    left: Rc::new(Box::new(IntegerLiteral {
+                        token: Token {
+                            token_type: TokenType::INT,
+                            literal: String::from("3"),
+                            line: 0,
+                        },
+                        value: 3.0,
+                    })),
+                    right: Box::new(IntegerLiteral {
+                        token: Token {
+                            token_type: TokenType::INT,
+                            literal: String::from("4"),
+                            line: 0,
+                        },
+                        value: 4.0,
+                    }),
+                })),
+                right: Box::new(Boolean {
+                    token: Token {
+                        token_type: TokenType::FALSE,
+                        literal: String::from("false"),
+                        line: 0,
+                    },
+                    value: false,
                 }),
             }),
         },
